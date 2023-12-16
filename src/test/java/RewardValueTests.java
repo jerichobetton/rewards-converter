6d8539293b2bdceef7b386d4de6fb929497083c8
Jericho Betton <jerichobetton@gmail.com>
2023-12-15 21:10:26 -0500

    Corrected Tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
+    public static final double MilesToCashRate = 0.0035;
 
     @Test
     void create_with_cash_value() {
@@ -20,11 +21,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        int calculatedMilesValue = (int)(cashValue / MilesToCashRate);
+        assertEquals(calculatedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double calculatedCashValue = milesValue * MilesToCashRate;
+        assertEquals(calculatedCashValue, rewardValue.getCashValue());
     }
 }
